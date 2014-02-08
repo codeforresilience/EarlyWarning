90279d1e0400a3cd1153de0b318db5e700199dd0
TAKANO Mitsuhiro <takano32@gmail.com>
2014-02-08 14:34:13 +0900

    twilio-ruby works.

--- example.rb
+++ example.rb
@@ -2,14 +2,27 @@
 
 require 'rubygems'
 require 'pit'
-require 'twilio'
+require 'twilio-ruby'
 
-config = Pit.get("twilio",
-                 :require => {
-  'sid' => 'twilio sid',
-  'token' => 'twilio auth token',
-})
+def call(config)
+  client = Twilio::REST::Client.new(config['sid'], config['token'])
 
+  from = config['from_phone_number']
+  client.account.calls.create(
+    from: from,
+    to:   '+81XXXXXXXXXX',
+    url:  'http://xxx.heroku.com/callback',
+  )
+end
 
+if $0 == __FILE__ then
+  config = Pit.get('twilio',
+                   :require => {
+    'sid'   => 'twilio sid',
+    'token' => 'twilio auth token',
+  })
+  config['from_phone_number'] = '+815031318881'
+  call(config)
+end
 
 
